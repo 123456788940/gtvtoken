18edc7235cf351a97ff92552e8e833e9ff61168e
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-04-04 22:34:02 +0530

    Update greentv.sol

--- greentv.sol
+++ greentv.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract gtvToken is ERC20 {
+    constructor() ERC20("Greentv Token", "GTV") {
+        _mint(msg.sender, 300 * 10 ** 6); // Set initial supply to 300 million tokens with 8 decimals
+    }
+
+    function _transfer(address to, uint256 numOfTokens) public returns (bool) {
+        require(numOfTokens > 0, "Number of tokens must be greater than 0");
+        require(numOfTokens <= balanceOf(msg.sender), "Insufficient balance");
+         _mint(to, numOfTokens);
+        _transfer(msg.sender, to, numOfTokens);
+        return true;
+    }
+}
 
